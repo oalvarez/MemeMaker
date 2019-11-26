1885275eb4c282bfe6a4d67e364d611d1127fa68
Oscar <oscar.al.hid@gmail.com>
2019-11-25 16:05:20 -0800

    Initial Commit

---
+++ MemeMaker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MemeMaker
+//
+//  Created by Oscar Alvarez Hidalgo on 11/25/19.
+//  Copyright © 2019 Oscar Alvarez Hidalgo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
